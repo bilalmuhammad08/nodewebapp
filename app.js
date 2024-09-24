a9e82b36e60f801eb73cbe386ff43737eb4ce184
bilalmuhammad08 <bilal-muhammad@BILAL.MUHAMMAD>
2024-09-24 13:01:24 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const env = require('dotenv').config();
 const app = express();
+const path = require('path')
 const db = require('./config/db')
+const userRouter = require('./routes/userRouter.js')
 db()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 
+
+app.set("view engine", "ejs");
+app.set("views",[path.join(__dirname,'views/users'),path.join(__dirname,'views/admin')])
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', userRouter)
 
 app.listen(process.env.PORT, ()=> console.log('server is running on 8085'));
 
