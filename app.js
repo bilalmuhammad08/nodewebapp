52bffa52c270d577800abebd0d050819867e0f7f
bilalmuhammad08 <bilal-muhammad@BILAL.MUHAMMAD>
2024-09-24 12:03:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const env = require('dotenv').config();
+const app = express();
+const db = require('./config/db')
+db()
+
+
+
+app.listen(process.env.PORT, ()=> console.log('server is running on 8085'));
+
+module.exports = app;
